a5d0d00946cafbee94d8798ef4825c50c0581e2e
RedRobin358 <77137838+RedRobin358@users.noreply.github.com>
2021-01-22 02:14:59 +0100

    Add files via upload

---
+++ seleniumTestMetod.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selinium
+{
+    class seleniumTestMetod
+    {
+        public static void EnterText(IWebDriver driver, string element, string value, string elementtype)
+        {
+            if (elementtype == "Id")
+            {
+                driver.FindElement(By.Id(element)).SendKeys(value);
+
+            }
+            else if (elementtype == "Name")
+            {
+                driver.FindElement(By.Name(element)).SendKeys(value);
+            }
+
+        }
+        public static void clickButton(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Id")
+            {
+                driver.FindElement(By.Id(element)).Click();
+
+            }
+            else if (elementtype == "Name")
+            {
+                driver.FindElement(By.Name(element)).Click();
+
+            }
+            
+
+        }
+        public static void dropDown(IWebDriver driver, string element, string value, string elementtype)
+        {
+            if (elementtype == "Id")
+            {
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+            }
+             if(elementtype=="Name")
+            {
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+            }
+             
+        }
+    }
+}
